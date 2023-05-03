ffddaa21c4d0f81d7fa298bef38bb36fd955ae2a
fhoward <francishoward28@gmail.com>
2023-05-03 16:40:13 +0800

    enhancement on UsersController - added error message on json and callback on create and update - added status_code

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,33 +1,42 @@
 class Api::V1::UsersController < ApplicationController
+    before_action :authorize_request, except: :create
     before_action :set_user, only: [:show, :update, :destroy]
+
     def index
         @users = User.all
-        render json: @users
+        render json: @users, status: :ok
     end
     
     def show
-        render json: @user
+        render json: @user, status: :ok
     end
     
     def create
-        
+        @user = User.new(user_params)
+        if @user.save
+            render json: @user, status: :created
+        else
+            render json: @user.errors.full_messages, status: :unprocessable_entity
+        end
     end
      
     def update
-        @user.update_attributes(user_params)
-        render json: @user 
+        if @user.update(user_params)
+            render json: @user,status: :ok
+        else
+            render json: @user.errors.full_messages, status: :unprocessable_entity
+        end
     end
     
     def destroy
         @user.destroy
-        render json: 'Success Delete'
+        render json: 'Success Delete',status: :ok
     end
 
     private
 
     def set_user
         @user = User.find(params[:id])
-        
     end
     
     def user_params
