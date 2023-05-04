3c5568cb7791a880ad3c5f7ffe4c6fd596c4a2db
fhoward <francishoward28@gmail.com>
2023-05-04 16:46:48 +0800

    add schema for friends_users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_03_070615) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_04_074406) do
   create_table "attendances", force: :cascade do |t|
     t.integer "user_id", null: false
     t.time "time_in"
@@ -18,6 +18,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_03_070615) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_attendances_on_user_id"
+  end
+
+  create_table "friends_users", id: false, force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "friend_id", null: false
+    t.index ["friend_id", "user_id"], name: "index_friends_users_on_friend_id_and_user_id"
+    t.index ["user_id", "friend_id"], name: "index_friends_users_on_user_id_and_friend_id"
   end
 
   create_table "users", force: :cascade do |t|
