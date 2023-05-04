94a491bdb225b1ccd939b3953d0d753b58a79544
fhoward <francishoward28@gmail.com>
2023-05-04 16:28:36 +0800

    remove friendship model - has_many through change to has_and_belongs_to_many

--- app/models/friendship.rb
+++
@@ -1,4 +0,0 @@
-class Friendship < ApplicationRecord
-  belongs_to :users, class_name: "User", foreign_key: "user_id"
-  belongs_to :friends ,class_name: 'User', foreign_key: "friend_id"
-end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
     has_many :attendances
-    has_many :friendships
-    has_many :friends, through: :friendships
+    has_and_belongs_to_many :friends,
+        class_name: "User", 
+        join_table: :friends_users, 
+        foreign_key: :user_id, 
+        association_foreign_key: :friend_id
     validates :name, presence: true, uniqueness: true
+
 end
