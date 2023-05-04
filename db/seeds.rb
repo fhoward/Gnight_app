da1745fba04ab7646f5a8a76282a6dfaad09f58a
fhoward <francishoward28@gmail.com>
2023-05-04 16:47:31 +0800

    add fake data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,23 @@
-20.times do
-    User.create(name: Faker::Name.name_with_middle)
+me = User.create(name: "Francisco Susa")
+
+30.times do
+    time_in = Faker::Time.forward(days: 23, period: :morning)
+    me.attendances.create(time_in: time_in, time_out: time_in + rand(8))
+end
+
+40.times do
+    user = User.create(name: Faker::Name.name_with_middle)
+
+    80.times do
+        time_in = Faker::Time.forward(days: 23, period: :morning)
+        user.attendances.create(time_in: time_in, time_out: time_in + rand(8))
+    end
+end
+
+_users = User.all 
+_users.each do |u|
+    10.times do
+        random_user = User.find(rand(1..41))
+        u.follow_friend(random_user)
+    end
 end
